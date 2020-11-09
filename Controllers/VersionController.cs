ca606fa8ac2740338e5177c4f4270adc36b31b03
Valty <valty123@mailru>
2020-11-09 09:54:04 +0700

    added log for GET and POST

--- Controllers/VersionController.cs
+++ Controllers/VersionController.cs
@@ -1,7 +1,7 @@
-﻿  
-using System.Reflection;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Mvc;
 using LAB1.Models;
+using Serilog;
 
 namespace LAB1.Controllers
 {
@@ -14,12 +14,21 @@ namespace LAB1.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
+            Log.Information("Acquiring version info");
+
+           Log.Warning("Some warning");
+
+           Log.Error("Here comes an error");
+
             var versionInfo = new Version
             {
                 Company = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyCompanyAttribute>().Company,
                 Product = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyProductAttribute>().Product,
                 ProductVersion = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
             };
+
+            Log.Information($"Acquired version is {versionInfo.ProductVersion}");
+            Log.Debug ($"Full version info: {@versionInfo}");
 
             return Ok(versionInfo);
         }
